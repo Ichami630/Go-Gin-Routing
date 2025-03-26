9bf284bfe9afad487bac73f92ec9c59773aaaef9
ichami630 <brandonichami@gmail.com>
2025-03-26 13:49:19 +0100

    basic and dynamic routing

--- main.go
+++ main.go
@@ -11,6 +11,15 @@ func main() {
 			"message": "Ok!!",
 		})
 	})
+	server.GET("/", func(c *gin.Context) {
+		c.String(200, "Welcome to Gin")
+	})
+
+	//dynamic route
+	server.GET("/user/:id", func(c *gin.Context) {
+		id := c.Param("id") //extract id from url
+		c.String(200, "User ID: %v", id)
+	})
 
 	server.Run(":8080") //listening on port 8080
 }
